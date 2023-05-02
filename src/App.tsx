e91c58bdfcbf81a5e2045a391c9c8798a00eb7d1
Milosska <110258611+Milosska@users.noreply.github.com>
2023-05-02 22:34:42 +0300

    CardComponent training

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import Card, { CardVariant } from "./components/Card";
 
 function App() {
   return (
     <div>
-     Hello!
+      <Card
+        width="200px"
+        height="200px"
+        variant={CardVariant.outlined}
+        onClick={(num) => {
+          console.log("Click! -", num);
+        }}
+      >
+        <p>Add some text!</p>
+        <button>Click me!</button>
+      </Card>
     </div>
   );
 }
